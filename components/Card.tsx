44c0fea14ff7ce92eddfdcb33e06cc65ff7d7568
lautaro <lautaroandreani2016@gmail.com>
2023-03-31 08:29:45 -0300

    styles: responsive

--- components/Card.tsx
+++ components/Card.tsx
@@ -2,35 +2,41 @@ import { Category, Item, SelectBallot } from "@/models/models"
 import Image from "next/image"
 
 type Props = {
-    item: Item
-    selectBallot?: SelectBallot
-    ballot: Category
-    handleSelect: (itemId: string, ballot: Category) => void 
+  item: Item
+  selectBallot?: SelectBallot
+  ballot: Category
+  handleSelect: (itemId: string, ballot: Category) => void
 }
 
-function Card({item, selectBallot, ballot, handleSelect, ...props }: Props) {
-    return (
-        <div className="flex flex-col gap-4 w-full">
-            <div className={`min-h-[35rem] max-w-[30rem] bg-nominee-color hover:bg-nominee-hover transition flex items-center flex-col justify-between rounded-md 
-            ${selectBallot && selectBallot[ballot.title]?.id === item.id ? 'border-4 border-nominee-color border-separate bg-nominee-hover' : ''}`}>
-            <span className="flex justify-center items-center flex-col">
-                <p className="text-lg font-medium p-2 text-center truncate w-9/12">{item?.title}</p>
-                <Image
-                src={item.photoUrL}
-                alt={item.title}
-                width={300}
-                height={400}
-                placeholder="blur"
-                blurDataURL={item.photoUrL}
-                className="rounded-full w-4/5 h-auto"
-                />
-            </span>
-          <button className="bg-yellow-500 py-2 px-4 rounded-sm w-full" onClick={() => handleSelect(item.id, ballot)}>
-            Select
-          </button>
-        </div>
+function Card({ item, selectBallot, ballot, handleSelect, ...props }: Props) {
+  return (
+    <div className='flex flex-col gap-4 w-full'>
+      <div
+        className={`min-h-[35rem] max-w-[30rem] bg-nominee-color hover:bg-nominee-hover transition flex items-center flex-col justify-between rounded-md overflow-hidden 
+            ${
+              selectBallot && selectBallot[ballot.title]?.id === item.id
+                ? "border-4 border-nominee-color border-separate bg-nominee-hover"
+                : ""
+            }`}
+      >
+        <span className='flex justify-center items-center flex-col'>
+          <p className='text-lg font-medium p-2 text-center truncate w-8/12'>{item?.title}</p>
+          <Image
+            src={item.photoUrL}
+            alt={item.title}
+            width={300}
+            height={400}
+            placeholder='blur'
+            blurDataURL={item.photoUrL}
+            className='h-auto'
+          />
+        </span>
+        <button className='bg-yellow-500 py-2 px-4 rounded-sm w-full' onClick={() => handleSelect(item.id, ballot)}>
+          Select
+        </button>
       </div>
-    )
+    </div>
+  )
 }
 
 export default Card
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,71 +1,72 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react"
 
-import { Category, Data, Item, SelectBallot } from "@/models/models";
-import { getBallotData } from "@/api/api";
-import Card from "@/components/Card";
+import { Category, Data, Item, SelectBallot } from "@/models/models"
+import { getBallotData } from "@/api/api"
+import Card from "@/components/Card"
 
 export default function Home() {
-  const [ballot, setBallot] = useState<Data>();
+  const [ballot, setBallot] = useState<Data>()
   const [selectBallot, setSelectBallot] = useState<SelectBallot>()
   const [toggleButton, setToggleButton] = useState<boolean>(false)
+  const [loading, setLoading] = useState<null | boolean>(null)
 
   useEffect(() => {
     async function fetchBallotData() {
       try {
+        setLoading(true)
         const response = await getBallotData()
+        setLoading(false)
         setBallot(response)
       } catch (error) {
-        if(error instanceof Error) console.error(error.message);
+        if (error instanceof Error) console.error(error.message)
       }
     }
 
     fetchBallotData()
-  }, []);
+  }, [])
 
   useMemo(() => {
-    if(selectBallot) {
+    if (selectBallot) {
       const toggleButton = Object.keys(selectBallot).length === ballot?.items.length
       setToggleButton(toggleButton)
     }
   }, [selectBallot])
 
-  const handleSelect = (itemId: Item['id'], ballot: Category) => {
+  const handleSelect = (itemId: Item["id"], ballot: Category) => {
     const getSelectedItem = ballot.items.find((nominee) => nominee.id === itemId)
 
-    if(getSelectedItem) {
+    if (getSelectedItem) {
       setSelectBallot((prev) => {
-        return {...prev, [ballot.title]: getSelectedItem }
+        return { ...prev, [ballot.title]: getSelectedItem }
       })
     }
   }
 
   return (
     <>
-      <h1 className="text-3xl font-bold uppercase text-center mt-4">
-        Awards 2021
-      </h1>
-      <main className="flex items-center flex-col p-4 relative">
+      <h1 className='text-3xl font-bold uppercase text-center mt-4'>Awards 2021</h1>
+      <main className='flex items-center flex-col p-4 relative'>
         {ballot &&
           ballot.items.map((ballot: Category) => (
-            <div key={ballot.id} className="w-3/5">
-              <h4 className="text-left text-2xl my-4 w-full bg-slate-800 rounded-md p-2">
-                {ballot.title}
-              </h4>
+            <div key={ballot.id} className='w-3/5'>
+              <h4 className='text-left text-2xl my-4 w-full bg-slate-800 rounded-md p-2'>{ballot.title}</h4>
 
-              <section className="grid grid-cols-3 gap-4">
+              <section className='grid grid-cols-1 xl:grid-cols-3 md:grid-cols-2 gap-4'>
                 {ballot.items.map((item: Item) => (
-                  <Card ballot={ballot} item={item} handleSelect={handleSelect} selectBallot={selectBallot} key={item.id}/>
+                  <Card ballot={ballot} item={item} handleSelect={handleSelect} selectBallot={selectBallot} key={item.id} />
                 ))}
               </section>
             </div>
           ))}
         {ballot && (
-          <button className="bg-nominee-color hover:bg-nominee-hover transition py-2 px-4 rounded-sm bottom-4 sticky ml-auto text-lg disabled:bg-slate-400 disabled:cursor-not-allowed" 
-          disabled={!toggleButton}>
+          <button
+            className='bg-nominee-color hover:bg-nominee-hover transition py-2 px-4 rounded-sm bottom-4 sticky ml-auto text-lg disabled:bg-slate-400 disabled:cursor-not-allowed'
+            disabled={!toggleButton}
+          >
             Submit Ballot
           </button>
         )}
       </main>
     </>
-  );
+  )
 }
